d99eb1739d7b889d123d4fcde2b67d2e72aaced3
kauebrpe <kauecarvnascimento@gmail.com>
2026-09-18 16:11:20 -0300

    atv4

--- 2ª_Periodo/07-Unidade-de-Extensao-Back-End/01-Aulas/04_repeticao.py
+++ 2ª_Periodo/07-Unidade-de-Extensao-Back-End/01-Aulas/04_repeticao.py
@@ -1,1 +1,33 @@
-for numero in range ()
+# Bloco = Precisa ter instrucoes dentro da sua estrutura = (While, For, If, Funcoes) = Sao blocos
+# Para + variavel + no + alcance + (0,10,2) + faca
+
+for i in range (1, 10, 2): # Inicio, Final, Incremento
+    print(i)
+
+#Estrurura de repeticao = While Loop Infinito
+
+while True:
+    calcular = input("1 - somar ou 2 - subtrair ")
+    if calcular == "1":
+        n1= int(input("Informe o primeiro numero: "))
+        n2= int(input("Informe o segundo numero: "))
+        print(f"A soma de {n1} e {n2} é {n1 + n2}")
+        break
+    elif calcular == "2":
+        n1= int(input("Informe o primeiro numero: "))
+        n2= int(input("Informe o segundo numero: "))
+        print(f"A subtracao de {n1} e {n2} é {n1 - n2}")
+        break
+    else:
+        print("Opcao invalida, tente novamente.")
+        break
+
+#Manipulacao de Strings
+texto = '''
+        Oi Oi, tudo bem?
+        Oi Oi, tudo bem?
+        Oi Oi, tudo bem?
+        Oi Oi, tudo bem?
+        '''
+
+texto = 'texto exemplo'
