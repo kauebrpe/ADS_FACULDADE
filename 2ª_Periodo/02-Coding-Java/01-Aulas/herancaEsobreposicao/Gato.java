731fcb696d2b16d5e1ec2e88a4a301648c22971a
Kauê de Carvalho Nascimento <kauecarvnascimento@gmail.com>
2026-09-20 17:58:41 -0300

    Add Gato class extending Animal with methods

---
+++ 2ª_Periodo/02-Coding-Java/01-Aulas/herancaEsobreposicao/Gato.java
@@ -0,0 +1,12 @@
+package herancaEsobreposicao;
+
+public class Gato extends Animal {
+    @Override
+    public void emitirSom() {
+        System.out.println("Miau miau");
+    }
+
+    public void arranharMoveis() {
+        System.out.println("Arranhar moveis");
+    }
+}
