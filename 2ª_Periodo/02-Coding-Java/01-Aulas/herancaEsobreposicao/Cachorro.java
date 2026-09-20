8b10ad8a95dcf449038e09ee682d96149e407ccc
Kauê de Carvalho Nascimento <kauecarvnascimento@gmail.com>
2026-09-20 17:58:08 -0300

    Create Cachorro.java

---
+++ 2ª_Periodo/02-Coding-Java/01-Aulas/herancaEsobreposicao/Cachorro.java
@@ -0,0 +1,12 @@
+package herancaEsobreposicao;
+
+public class Cachorro extends Animal {
+    @Override
+    public void emitirSom() {
+        System.out.println("Au au");
+    }
+
+    public void abanaRabo() {
+        System.out.println("Abana rabo");
+    }
+}
